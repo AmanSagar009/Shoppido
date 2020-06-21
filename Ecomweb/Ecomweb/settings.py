6a813e12cc589facc13a97bda28a4b82ad86169f
Aman Sagar <09amansagar@gmail.com>
2020-06-21 23:12:28 +0530

    third commit

--- Ecomweb/Ecomweb/settings.py
+++ Ecomweb/Ecomweb/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'Ecomweb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['Ecomweb/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
